6df8766584d78b680561aed3344cf249fbc81447
Ben Hutton <ben@benhutton.com.au>
2020-10-27 22:19:24 +0800

    Saves existing firstname when submitted

--- appointments/src/CustomerForm.jsx
+++ appointments/src/CustomerForm.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-export const CustomerForm = ({firstName}) => (
-    <form id="customer">
-      <label htmlFor="firstName">First name</label>
-      <input type="text" id="firstName" name="firstName" value={firstName} readOnly />
-    </form>
-);
+export const CustomerForm = ({ firstName, onSubmit }) => {
+    const customer = { firstName };
+    return (
+        <form id="customer" onSubmit={() => onSubmit(customer)}>
+            <label htmlFor="firstName">First name</label>
+            <input type="text" id="firstName" name="firstName" value={firstName} readOnly />
+        </form >
+    );
+};
